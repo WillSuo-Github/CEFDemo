85a591202a7773bc912be4141280c0b5ab9c599d
will <willsuosuo@gmail.com>
2025-02-21 17:49:50 +0800

    feat: finally we make the browser view look like a normal web view

--- AppDelegate.mm
+++ AppDelegate.mm
@@ -1,24 +1,22 @@
 #import "AppDelegate.h"
+#include "include/cef_browser.h"
+#include "include/cef_command_line.h"
+#include "include/views/cef_browser_view.h"
+#include "include/views/cef_window.h"
+#include "include/views/cef_view.h"
+#include "include/wrapper/cef_helpers.h"
+
 #include "include/cef_app.h"
 #include "include/cef_browser.h"
 #include "include/cef_command_line.h"
 #include "include/cef_client.h"
-#include "include/cef_version.h"
+#include "tests/cefsimple/simple_handler.h"
 #include "ClientApp.h"
-
 #import <AppKit/AppKit.h>
-
-// 用来作为浏览器的 "client"。这里只是一个简单示例。
-class SimpleCefClient : public CefClient {
-public:
-    SimpleCefClient() {}
-    // 可根据需要自定义各种 Handler。
-    IMPLEMENT_REFCOUNTING(SimpleCefClient);
-};
 
 @interface AppDelegate()
 @property (strong, nonatomic) NSWindow *window;
-@property (strong, nonatomic) NSTimer *cefTimer;  // 用于定时调用 CefDoMessageLoopWork
+@property (strong, nonatomic) NSTimer *cefTimer;
 @end
 
 @implementation AppDelegate
@@ -26,46 +24,45 @@ public:
 @synthesize cefTimer = _cefTimer;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
-    // 创建 Cef 的 ClientApp 实例
     CefRefPtr<ClientApp> app(new ClientApp);
-
+    
     CefMainArgs main_args;
     CefSettings settings;
-    // 设置使用外部消息泵，这样就不必调用阻塞式的 CefRunMessageLoop
     settings.external_message_pump = true;
     
     CefInitialize(main_args, settings, app.get(), NULL);
-
-    // 准备创建浏览器窗口
-    CefWindowInfo windowInfo;
-    CefBrowserSettings browserSettings;
     
-    // 从命令行中获取 URL（如果有传递 "url" 参数）
+    CefRefPtr<CefCommandLine> command_line =
+    CefCommandLine::GetGlobalCommandLine();
+
+    CefBrowserSettings browser_settings;
+    
     std::string url;
-    CefRefPtr<CefCommandLine> command_line = CefCommandLine::GetGlobalCommandLine();
-    if (command_line->HasSwitch("url")) {
-        url = command_line->GetSwitchValue("url");
-    }
+    
+
+    url = command_line->GetSwitchValue("url");
     if (url.empty()) {
-        // 默认打开 Google 首页
         url = "https://www.google.com";
     }
     
-    // 获取当前 NSWindow 的 contentView
+
+    CefWindowInfo windowInfo;
+    CefBrowserSettings browserSettings;
+    windowInfo.runtime_style = CEF_RUNTIME_STYLE_ALLOY;
+    
     NSView *contentView = self.window.contentView;
     CefRect rect(0, 0, contentView.frame.size.width, contentView.frame.size.height);
     // 设置为子窗口，使 CEF 的浏览器嵌入到 contentView 中
     windowInfo.SetAsChild((__bridge void *)contentView, rect);
-    
-    // 创建浏览器（这里没有设置自定义的 CefClient，如果需要可以传入 SimpleCefClient）
     CefBrowserHost::CreateBrowser(windowInfo, nullptr, url, browserSettings, nullptr, nullptr);
-
+//
     // 使用 NSTimer 周期性地调用 CefDoMessageLoopWork，集成到 Cocoa 的主事件循环中
     self.cefTimer = [NSTimer scheduledTimerWithTimeInterval:0.01
                                                      target:self
                                                    selector:@selector(cefDoMessageLoopWork:)
                                                    userInfo:nil
                                                     repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:self.cefTimer forMode:NSRunLoopCommonModes];
 }
 
 // 定时器回调方法：驱动 CEF 的消息循环
--- main.mm
+++ main.mm
@@ -1,11 +1,12 @@
 #import <Cocoa/Cocoa.h>
 #import "AppDelegate.h"
-#import "include/wrapper/cef_library_loader.h"
+#include "include/wrapper/cef_library_loader.h"
+#include "include/cef_sandbox_mac.h"
 
 int main(int argc, const char * argv[]) {
     CefScopedLibraryLoader library_loader;
     if (!library_loader.LoadInMain()) {
-      return 1;
+        return 1;
     }
     
     [NSApplication sharedApplication];
